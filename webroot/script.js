7d2b5c86b08cc45aefece3a94d330d2a822a79bf
Rinon TENDRINOMENA <rinon.ten@onja.org>
2020-09-10 09:12:03 +0300

    Sliding the image in portfolio by applying javascript to it and I had to modify the list of images in html as well. Applying some style in order to show the image when clicked in the portfolio file

--- webroot/script.js
+++ webroot/script.js
@@ -3,6 +3,36 @@ const main = document.querySelector("main");
 const menu = document.querySelector(".page_navigation");
 const openMenu = document.querySelector(".open_menu");
 const heading = document.querySelector(".page_header_heading");
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("image_slides");
+  var dots = document.getElementsByClassName("image_slider"); 
+//   var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  // captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
 // A function that displays the menu
 const showMenu = () => {
   menu.classList.remove('close');
